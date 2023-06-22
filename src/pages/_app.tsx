d14c6b9c7b7befb82e67f20219527212743e04ed
JayCode Developer <JayCode.contakt@gmail.com>
2023-06-22 02:40:19 +0200

    Integrated Alert with app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { AlertProvider } from "@/hooks/useAlert";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AlertProvider>
+      <Component {...pageProps} />
+    </AlertProvider>
+  );
 }
